abc121118c709008fdd67b1129889a0419da185a
TehChroma <32431206+TehChroma@users.noreply.github.com>
2017-12-05 20:49:20 +0000

    Add files via upload

---
+++ INIFileReaderWriter/IniFileReaderWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Runtime.InteropServices;
+
+namespace INIFileReaderWriter
+{
+    class IniFileReaderWriter
+    {
+        public string path;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        //INI Constructor.
+        public IniFileReaderWriter(string INIPath)
+        {
+            path = INIPath;
+        }
+
+        //Writes To The INI File.
+        public void INIWriteValue(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, this.path);
+        }
+
+        //Reads From The INI File.
+        public string INIReadValue(string Section, string Key)
+        {
+            StringBuilder temp = new StringBuilder(255);
+            int I = GetPrivateProfileString(Section, Key, "", temp, 255, this.path);
+            return temp.ToString();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+#INIFileReaderWriter, An Easy To Use Open Source INI Reader / Writer For C# Applicatons
+
+Examples:
+
+Reading INI File:
+ini.IniReadValue("[Section], [Key]");
+
+Writing To INI File:
+ini.IniWriteValue("[Section], [Key]");
